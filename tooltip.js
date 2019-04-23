303e6db6b33a541e91bbf54056e1dd5a2a06e825
Simon Davis <simonfreelance1@gmail.com>
2019-04-23 16:23:49 -0400

    Able to add custom attributes

--- tooltip.js
+++ tooltip.js
@@ -2,12 +2,14 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
-    console.log("tooltip");
+    this._tooltipText = "None";
   }
   connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
+    }
     const tooltipIcon = document.createElement("span");
     tooltipIcon.textContent = " (?)";
-    console.log(this._showTooltip);
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
@@ -15,7 +17,7 @@ class Tooltip extends HTMLElement {
 
   _showTooltip() {
     this._tooltipContainer = document.createElement("div");
-    this._tooltipContainer.textContent = "This is the tooltip text!";
+    this._tooltipContainer.textContent = this._tooltipText;
     this.appendChild(this._tooltipContainer);
   }
   _hideTooltip() {
