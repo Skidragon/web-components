b76dff0ef04756ed86e6722a00ec0767aee60e5c
Simon Davis <simonfreelance1@gmail.com>
2019-04-23 17:27:40 -0400

    Added styles to custom html element

--- tooltip.js
+++ tooltip.js
@@ -2,29 +2,42 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
-    this._tooltipText = "None";
-    this.attachShadow({ mode: "open" });
-    const template = document.querySelector("#tooltip-template");
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this._tooltipText = 'Some dummy tooltip text.';
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+        <style>
+            div {
+                background-color: black;
+                color: white;
+                position: absolute;
+                z-index: 10;
+            }
+        </style>
+        <slot>Some default</slot>
+        <span> (?)</span>
+    `;
   }
+
   connectedCallback() {
-    if (this.hasAttribute("text")) {
-      this._tooltipText = this.getAttribute("text");
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
     }
-    const tooltipIcon = this.shadowRoot.querySelector("span");
-    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
-    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
+    const tooltipIcon = this.shadowRoot.querySelector('span');
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   _showTooltip() {
-    this._tooltipContainer = document.createElement("div");
+    this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
     this.shadowRoot.appendChild(this._tooltipContainer);
   }
+
   _hideTooltip() {
     this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
-customElements.define("uc-tooltip", Tooltip);
+customElements.define('uc-tooltip', Tooltip);
